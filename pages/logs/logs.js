4962c2482dd49b54ca3d0bd7e0d0282bd5bb1032
linzs148 <1483100349@qq.com>
2021-05-11 17:15:52 +0800

    linzs: init

---
+++ pages/logs/logs.js
@@ -0,0 +1,31 @@
+// logs.js
+const util = require('../../utils/util.js')
+/*
+Page({
+	data: {
+		logs: []
+	},
+	onLoad() {
+		this.setData({
+			logs: (wx.getStorageSync('logs') || []).map(log => {
+				return {
+					date: util.formatTime(new Date(log)),
+					timeStamp: log
+				}
+			})
+		})
+	}
+})
+*/
+Page({
+	data: {
+		date: {}
+	},
+	onLoad() {
+		util.processTime(new Date()).then(
+			res => this.setData({
+				date: res
+			})
+		)
+	}
+})
---
+++ utils/util.js
@@ -0,0 +1,43 @@
+/*
+根据公历时间调用黄历API
+response.data.newslist[0]结构
+{
+	gregoriandate: 公历日期
+	lubarmonth: 农历月
+	lunarday: 农历日
+	lunar_festival: 农历节日
+	festival: 公历节日
+	fitness: 适宜
+	taboo: 不宜
+	tiangandizhiyear: 天干地支年
+	tiangandizhimonth: 天干地支月
+	tiangandizhiday: 天干地支日
+	jieqi: 节气
+}
+*/
+
+const processTime = function(date) {
+	const year = date.getFullYear()
+	const month = date.getMonth() + 1
+	const day = date.getDate()
+
+	return new Promise((resolve, reject) => {
+		wx.request({
+			url: "https://api.tianapi.com/txapi/lunar/index",
+	  		data: {
+		  		key: "b6cafa58c55c3bea259b91aa7101796e",
+				date: `${year}-${month}-${day}`
+			},
+			success: function(res) {
+				resolve(res.data.newslist[0])
+			},
+			fail: function(error) {
+				reject(error)
+			}
+		})
+	})
+}
+
+module.exports = {
+	processTime
+}
