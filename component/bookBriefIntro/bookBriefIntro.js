473abdb25cba7c3f6d8c7e6e2d4564c4dec4ab8a
suyiiis <1225748424@qq.com>
2021-05-25 17:26:49 +0800

    框架

---
+++ component/bookBriefIntro/bookBriefIntro.js
@@ -0,0 +1,70 @@
+// component/bookBriefIntro/bookBriefIntro.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+     id:0,
+     src:"https://img-for-se.oss-cn-shanghai.aliyuncs.com/qh1.png",
+     name:"雀魂",
+     rate:10,
+     briefIntro:"我超开心的！我超开心的！我超开心的！我超开心的！我超开心的！我超开心的！我超开心的！我超开心的！"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
